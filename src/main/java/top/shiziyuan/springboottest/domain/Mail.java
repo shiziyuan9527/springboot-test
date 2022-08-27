e69f8ee1d25d4f3b21ed56b98a9f130b7f0e324d
li <1920727772@qq.com>
2022-08-27 11:21:14 +0800

    mail

---
+++ src/main/java/top/shiziyuan/springboottest/domain/Mail.java
@@ -0,0 +1,14 @@
+package top.shiziyuan.springboottest.domain;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class Mail {
+    private String from;
+    private String to;
+    private String subject;
+    private String text;
+    private MultipartFile file;
+    private String filePath;
+}
